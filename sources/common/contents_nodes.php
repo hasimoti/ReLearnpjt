0eaa7b386078b71e52947efb724c61a0a6e9281c
soutahasimoto <hasimoto.s511@gmail.com>
2025-05-14 10:03:39 +0900

    ここがtop

--- sources/common/contents_nodes.php
+++ sources/common/contents_nodes.php
@@ -46,10 +46,52 @@ class cheader extends cnode {
 <title>PHPBase2サンプル</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    
+      <!-- ハンバーガーメニュー部分 -->
+      <div class="nav">
+    
+        <!-- ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
+        <input id="drawer_input" class="drawer_hidden" type="checkbox">
+    
+        <!-- ハンバーガーアイコン -->
+        <label for="drawer_input" class="drawer_open"><span></span></label>
+    
+        <!-- メニュー -->
+        <nav class="nav_content">
+          <ul class="nav_list">
+            <li class="nav_item"><a href="">メニュー1</a></li>
+            <li class="nav_item"><a href="">メニュー2</a></li>
+            <li class="nav_item"><a href="">メニュー3</a></li>
+          </ul>
+        </nav>
+   
+      </div>
+
+     <!-- 検索バー部分-->
+	 <form action="#" class="search-form-5">
+		<label>
+			<input type="text" placeholder="キーワードを入力">
+		</label>
+		<button type="submit" aria-label="検索"><img src="img/icon_108190_256.png" width="40" ></button>
+	</form>
+<!--アイコン部分-->
+	<div class="aikon">
+	<button type="submit" aria-label="aikon"><img src="img/images.png" width="40" ></button>
+		
+	</div>
+
+	<!--設定部分-->
+	<div class="settei">
+		<img src="img/settei.png" width="40" >
+	</div>
+
 </head>
 <body>
 <!-- 全体コンテナ　-->
 <div class="container">
+
+
 
 </header>
 END_BLOCK;
@@ -110,6 +152,7 @@ class cside_header extends cnode {
 <div class="container-fluid">
 <header class="navbar sticky-top bg-secondary-subtle flex-md-nowrap p-0 shadow" >
 <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-dark" href="#">サイト名</a>
+
 </header>
 	<!-- 行　-->
 	<div class="row">
