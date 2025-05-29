991c66e5faa445167a7b0d6e5a94805a77093d3d
soutahasimoto <hasimoto.s511@gmail.com>
2025-05-29 13:38:57 +0900

    パスワードとか変えた

--- sources/common/config.php
+++ sources/common/config.php
@@ -11,13 +11,13 @@ define('DB_RDBMS','mysql');
 //MySQLの場合のキャラ設定にSET NAMESを使用するかどうか
 define('DB_MYSQL_SET_NAMES','1');
 //ホスト(ローカルの場合は'localhost'と記述)
-define('DB_HOST','*********');
+define('DB_HOST','localhost');
 //ユーザー
-define('DB_USER','*********');
+define('DB_USER','j2025bdb');
 //パスワード
-define('DB_PASS','*********');
+define('DB_PASS','9yafMZ9YCfg1S16k!');
 //DB名
-define('DB_NAME','*********');
+define('DB_NAME','j2025bdb');
 //DBのキャラセット
 define('DB_CHARSET','utf8');
 //PHPのキャラセット
