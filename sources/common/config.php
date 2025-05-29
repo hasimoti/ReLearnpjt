3ebde42b44e5260ebb2019c2ab904583a02368f7
soutahasimoto <hasimoto.s511@gmail.com>
2025-05-29 13:33:49 +0900

    phpbase2更新

--- sources/common/config.php
+++ sources/common/config.php
@@ -11,13 +11,13 @@ define('DB_RDBMS','mysql');
 //MySQLの場合のキャラ設定にSET NAMESを使用するかどうか
 define('DB_MYSQL_SET_NAMES','1');
 //ホスト(ローカルの場合は'localhost'と記述)
-define('DB_HOST','localhost');
+define('DB_HOST','*********');
 //ユーザー
-define('DB_USER','root');
+define('DB_USER','*********');
 //パスワード
-define('DB_PASS','password');
+define('DB_PASS','*********');
 //DB名
-define('DB_NAME','phpbase2');
+define('DB_NAME','*********');
 //DBのキャラセット
 define('DB_CHARSET','utf8');
 //PHPのキャラセット
