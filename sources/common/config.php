173e6cd8b21b95a6dda1d19f883fe8e2193cb73b
soutahasimoto <hasimoto.s511@gmail.com>
2025-06-02 13:33:39 +0900

    user名変わってたなんで

--- sources/common/config.php
+++ sources/common/config.php
@@ -13,11 +13,11 @@ define('DB_MYSQL_SET_NAMES','1');
 //ホスト(ローカルの場合は'localhost'と記述)
 define('DB_HOST','localhost');
 //ユーザー
-define('DB_USER','root');
+define('DB_USER','j2025bdb');
 //パスワード
-define('DB_PASS','password');
+define('DB_PASS','9yafMZ9YCfg1S16k!');
 //DB名
-define('DB_NAME','phpbase2');
+define('DB_NAME','j2025bdb');
 //DBのキャラセット
 define('DB_CHARSET','utf8');
 //PHPのキャラセット
