1edcfb591e2ec5b65c9d05e985d059338fd09350
kaito <zeal23310083@fsgcl.jp>
2025-06-09 13:53:54 +0900

    ヘッダーの追加

--- sources/admin/admin_dashboard.php
+++ sources/admin/admin_dashboard.php
@@ -10,8 +10,33 @@ if (empty($_SESSION['is_admin'])) {
 <head>
     <meta charset="utf-8">
     <title>管理者ダッシュボード</title>
+    <style>
+        .header {
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            background: #333;
+            padding: 10px 20px;
+        }
+        .header a {
+            color: #fff;
+            text-decoration: none;
+            margin-left: 30px;
+            font-weight: bold;
+        }
+        .header a:hover {
+            text-decoration: underline;
+        }
+    </style>
 </head>
 <body>
+    <div class="header">
+        <a href="log.php">ログ</a>
+        <a href="test_manage.php">テスト管理</a>
+        <a href="admin_login_manage.php">ログイン情報</a>
+        <a href="course_manage.php">講座管理</a>
+        <a href="admin_dashboard.php">ホーム</a>
+    </div>
     <h1>管理者ダッシュボード</h1>
     <p>ようこそ、管理者さん！</p>
 </body>
