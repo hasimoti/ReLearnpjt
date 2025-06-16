9b6457e628f6d717bd6d16f68d89570a857c3687
kaito <zeal23310083@fsgcl.jp>
2025-06-16 14:17:18 +0900

    先生のサンプルを追加

---
+++ sources/admin_sample/index.php
@@ -0,0 +1,95 @@
+<?php
+/*!
+@file index.php
+@brief メインメニュー
+@copyright Copyright (c) 2024 Yamanoi Yasushi.
+*/
+
+//ライブラリをインクルード
+require_once("../common/libs.php");
+//以下はセッション管理用のインクルード
+require_once("../common/auth_adm.php");
+
+
+//--------------------------------------------------------------------------------------
+///	本体ノード
+//--------------------------------------------------------------------------------------
+class cmain_node extends cnode {
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief	コンストラクタ
+	*/
+	//--------------------------------------------------------------------------------------
+	public function __construct() {
+		//親クラスのコンストラクタを呼ぶ
+		parent::__construct();
+	}
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief	構築時の処理(継承して使用)
+	@return	なし
+	*/
+	//--------------------------------------------------------------------------------------
+	public function create(){
+	}
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief  表示(継承して使用)
+	@return なし
+	*/
+	//--------------------------------------------------------------------------------------
+	public function display(){
+//PHPブロック終了
+?>
+<!-- コンテンツ　-->
+<div class="contents">
+<h5><strong>メインメニュー</strong></h5>
+ようこそ<?= get_admin_name(); ?>さん
+<table class="table table-bordered">
+<thead>
+<tr>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td><a href="member_list.php" class="nav-link link-success">メンバー管理</a></td>
+</tr>
+<tr>
+<td><a href="admin_master_list.php" class="nav-link link-success">管理者管理</a></td>
+</tr>
+<tr>
+<td><a href="mail_chk.php" class="nav-link link-success">メール送信テスト</a></td>
+</tr>
+</tbody>
+</table>
+</div>
+<!-- /コンテンツ　-->
+<?php 
+//PHPブロック再開
+	}
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief	デストラクタ
+	*/
+	//--------------------------------------------------------------------------------------
+	public function __destruct(){
+		//親クラスのデストラクタを呼ぶ
+		parent::__destruct();
+	}
+}
+
+//ページを作成
+$page_obj = new cnode();
+//ヘッダ追加
+$page_obj->add_child(cutil::create('cheader'));
+//本体追加
+$page_obj->add_child(cutil::create('cmain_node'));
+//フッタ追加
+$page_obj->add_child(cutil::create('cfooter'));
+//構築時処理
+$page_obj->create();
+//ページ全体を表示
+$page_obj->display();
+
+
+?>
